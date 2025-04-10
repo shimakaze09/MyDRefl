6454747c7e83d05469b57438398e81096b8e980e
John <yangtianzhuo970425@gmail.com>
2025-04-11 11:41:37 +1200

    Add initial configuration header for MyDRefl with MaxArgNum definition

---
+++ include/MyDRefl/config.h
@@ -0,0 +1,11 @@
+//
+// Created by Admin on 11/04/2025.
+//
+
+#pragma once
+
+#include <cstddef>
+
+namespace My::MyDRefl {
+static constexpr std::size_t MaxArgNum = 256;
+}  // namespace My::MyDRefl
