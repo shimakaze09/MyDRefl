112e797a0b9f8c85e6df6b89ec91a4a2ec2e4d20
John <yangtianzhuo970425@gmail.com>
2025-04-15 22:01:37 +1200

    Refactor: Standardize method flag handling in variant, optional, and dd_type tests

--- src/test/23_optional/main.cpp
+++ src/test/23_optional/main.cpp
@@ -21,6 +21,8 @@ int main() {
       case MethodFlag::Static:
         std::cout << "Static";
         break;
+      default:
+        continue;
     }
     std::cout << "]";
 
--- src/test/24_dd_type/main.cpp
+++ src/test/24_dd_type/main.cpp
@@ -101,6 +101,8 @@ int main() {
       case MethodFlag::Static:
         std::cout << "Static";
         break;
+      default:
+        continue;
     }
     std::cout << "]";
 
